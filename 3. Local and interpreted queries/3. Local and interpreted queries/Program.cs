d34b38b2104c067ec461218e51e4098f4c71b7db
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-23 19:38:48 +0100

    no se que hace esto

---
+++ 3. Local and interpreted queries/3. Local and interpreted queries/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+
+var xml = 
+    @"<ingredients>
+        <ingredient name='milk' quantity='200' price='2.99'/>
+        <ingredient name='sugar' quantity='100' price='4.99'/>
+        <ingredient name='safron' quantity='1' price='46.77'/>
+    </ingredients>"; 
+XElement xmlData = XElement.Parse(xml); 
+XElement milk = xmlData
+                .Descendants("ingredient")
+                .First(x => x.Attribute("name")
+                .Value == "milk");
+
+ XAttribute nameAttribute = milk.FirstAttribute; 
+// name attribute
+XAttribute priceAttribute = milk.Attribute("price");
+string priceOfMilk = priceAttribute.Value; // 2.99
+XAttribute quantity = milk
+                        .Attributes()
+                        .Skip(1)
+                        .First(); // quantity attribute
