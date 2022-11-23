853eaa63042fc4c14d11629f6aa47bdb60a170b7
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-23 17:43:05 +0100

    fibonacci acabado y mas pruebas

--- 1.fibonnaci/1.fibonnaci/Program.cs
+++ 1.fibonnaci/1.fibonnaci/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] fibonacci = { 0, 1, 1, 2, 3, 5 };
+// Scalar return value
+int numberOfElements = fibonacci.Count();
+Console.WriteLine("Count: {0}", numberOfElements);
+
+var x = fibonacci.Distinct();
+foreach (var item in x)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("///////");
+foreach (int element in fibonacci)
+{
+    Console.WriteLine(element);
+}
+
+// Output sequence return value
+IEnumerable<int> distinctNumbers = fibonacci.Distinct();
+Console.WriteLine("Elements in output sequence:");
+foreach(var number in distinctNumbers)
+{
+    Console.WriteLine(number);
+}
