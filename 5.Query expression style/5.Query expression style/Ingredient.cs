2f1325d552990c9c344bbaf09b05f370af2bd878
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-24 09:22:24 +0100

    query expressions entendido

---
+++ 5.Query expression style/5.Query expression style/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace QueryExpressionStyle
+{
+    public class Ingredient
+	{
+		public string Name { get; set; }
+		public int Calories{ get; set; }
+	}
+}
---
+++ 5.Query expression style/5.Query expression style/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+
+namespace QueryExpressionStyle
+{
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Ingredient[] ingredients = 
+				{ 
+					new Ingredient{ Name = "Sugar", Calories = 500 }, 
+					new Ingredient{ Name = "Egg", Calories = 100 }, 
+					new Ingredient{ Name = "Milk", Calories = 150 }, 
+					new Ingredient{ Name = "Flour", Calories = 50 }, 
+					new Ingredient{ Name = "Butter", Calories = 200 } 
+			}; 
+			
+			IEnumerable<string> highCalorieIngredientNamesQuery = 
+				from i in ingredients where i.Calories >= 150
+				orderby i.Name 
+				select i.Name; 
+			
+			foreach (var ingredientName in highCalorieIngredientNamesQuery) 
+			{  
+				Console.WriteLine(ingredientName); 
+			}
+		}
+	}
+}
