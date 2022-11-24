00ccdb03d42165a867fa1f82c907906740888ad9
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-24 10:24:47 +0100

    ej3 completado facil

---
+++ ej3-numbersAndSquare/ej3-numbersAndSquare/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+3. Write a program in C# Sharp to find the number of an array and the
+square of each number. 
+Expected Output :
+{ Number = 9, SqrNo = 81 }
+{ Number = 8, SqrNo = 64 }
+{ Number = 6, SqrNo = 36 }
+{ Number = 5, SqrNo = 25 } 
+ */
+
+namespace MyNamespace
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = { 3, 9, 2, 8, 6, 5 };
+
+			IEnumerable<int> square = from x in numbers
+									  where (x * x) > 20
+									  select x;
+
+			foreach (int num in square)
+			{
+				Console.WriteLine("Number = {0}, SqrNo = {1}", num, num * num);
+			}
+		}
+	}
+}
