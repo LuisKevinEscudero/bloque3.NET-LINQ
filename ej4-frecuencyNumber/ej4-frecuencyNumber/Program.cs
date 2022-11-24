f312d0f36e02079564b4af7e1fee6295492ed078
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-24 12:01:37 +0100

    falta el segundo metodo que el primero no lo entiendo

---
+++ ej4-frecuencyNumber/ej4-frecuencyNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Write a program in C# Sharp to display the number and frequency 
+of number from giving array. 
+Expected Output :
+The number and the Frequency are :
+Number 5 appears 3 times
+Number 9 appears 2 times
+Number 1 appears 1 times 
+ */
+
+namespace frecuencyNumber
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		public void method1()
+		{
+            int[] numbers = { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+			Console.Write("\nLINQ : Display the number and frequency of number from given array : \n");
+			Console.Write("---------------------------------------------------------------------\n");
+			Console.WriteLine("The numbers in array are: ");
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				Console.Write(numbers[i] + " ");
+			}
+
+			var numQuery = from x in numbers
+						   group x by x into y
+						   select y;
+			Console.WriteLine("\nThe number and the Frequency are : \n");
+			foreach (var arrNo in numQuery)
+			{
+				Console.WriteLine("Number " + arrNo.Key + " appears " + arrNo.Count() + " times");
+			}
+		}
+
+		public void method2()
+		{
+            int[] numbers = { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            Console.Write("\nLINQ : Display the number and frequency of number from given array : \n");
+            Console.Write("---------------------------------------------------------------------\n");
+            Console.WriteLine("The numbers in array are: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+	}
+}
