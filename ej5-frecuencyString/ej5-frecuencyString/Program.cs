663ec28935911974d6df0e2a97ee7d93e40d7981
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-24 16:42:50 +0100

    ej 5 facilito

---
+++ ej5-frecuencyString/ej5-frecuencyString/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Write a program in C# Sharp to display the characters 
+and frequency of character from giving string. Go to the editor
+Test Data:
+Input the string: apple
+Expected Output:
+The frequency of the characters are :
+Character a: 1 times
+Character p: 2 times
+Character l: 1 times
+Character e: 1 times 
+ */
+
+namespace FrecuencyString
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string word = "apple";
+
+			IEnumerable<char> characters = word.Distinct();
+			foreach (var item in characters)
+			{
+				int frecuency = word.Count(x => x == item);
+				Console.WriteLine("Character {0}: {1} times", item, frecuency);
+			}
+		}
+	}
+}
