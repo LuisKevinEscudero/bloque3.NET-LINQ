b17ca16bd96f13268d91f29f59293e801a03cc2c
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-25 18:11:25 +0100

    ej6 facil

---
+++ ej6-dayNames/ej6-dayNames/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Write a program in C# Sharp to display the name of the days of a week.
+Expected Output:
+Sunday
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday
+Saturday 
+ */
+
+namespace DayNames
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+			Console.Write("\nLINQ : Display the name of the days of a week : ");
+			Console.Write("\n------------------------------------------------\n");
+
+			IEnumerable<string> day = from d in dayWeek
+									  select d;
+
+			foreach (var item in day)
+			{
+				Console.WriteLine(item);
+			}
+			
+			
+		}
+	}
+}
