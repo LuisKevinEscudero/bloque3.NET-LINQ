8eb3d71a1b3c22276aeb33cdd2ebf56169665743
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-29 09:11:49 +0100

    ej9 facilito

---
+++ ej9-listOfNumbers/ej9-listOfNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Write a program in C# Sharp to create a list of numbers 
+and display the numbers greater than 80 as output.
+Test Data :
+The members of the list are :
+55 200 740 76 230 482 95
+Expected Output :
+The numbers greater than 80 are :
+200
+740
+230
+482
+95 
+ */
+
+namespace ListOfNumbers
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> numbers = new List<int>();
+			numbers.Add(55);
+			numbers.Add(200);
+			numbers.Add(740);
+			numbers.Add(76);
+			numbers.Add(230);
+			numbers.Add(482);
+			numbers.Add(95);
+
+			Console.Write("\nLINQ : Create a list of numbers and display the numbers greater than 80 : ");
+			Console.Write("\n-------------------------------------------------------------------------\n");
+			Console.WriteLine("\nThe members of the list are : ");
+		   
+			foreach (var num in numbers)
+			{
+				Console.Write("{0} ", num);
+			}
+
+			IEnumerable<int> greaterNumbers = from x in numbers
+											  where x > 80
+											  select x;
+
+			Console.WriteLine("\n");
+			foreach (var item in greaterNumbers)
+			{
+				Console.WriteLine("{0} is greater than 80", item);
+			}
+		}
+	}
+}
