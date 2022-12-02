c7c449661d2707b2960de9695bd80c6de5c3e5d0
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-02 12:45:08 +0100

    ej29 completado aunque no entiendo del todo el group

---
+++ ej29-groupOfCities/ej29-groupOfCities/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Write a program in C# Sharp to split a collection of strings into some groups. Go to the editor
+Expected Output :
+Here is the group of cities :
+
+ROME; LONDON; NAIROBI
+-- here is a group of cities --
+
+CALIFORNIA; ZURICH; NEW DELHI
+-- here is a group of cities --
+
+AMSTERDAM; ABU DHABI; PARIS
+-- here is a group of cities --
+
+NEW YORK
+-- here is a group of cities --*/
+namespace GroupOfCities
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] cities =
+			{
+				"ROME","LONDON","NAIROBI","CALIFORNIA",
+				"ZURICH","NEW DELHI","AMSTERDAM",
+				"ABU DHABI", "PARIS","NEW YORK"
+			};
+
+			Console.Write("\nLINQ : Split a collection of strings into some groups of 3 cities  : ");
+			Console.Write("\n-------------------------------------------------------\n");
+			Console.WriteLine("Here is the group of cities :");
+			Console.WriteLine(String.Join(", ", cities));
+
+			IEnumerable<string> groupCities = from city in cities
+											  group city by city.Length into cityGroup
+											  select cityGroup.Key + " - " + String.Join(", ", cityGroup);
+			Console.WriteLine("+++++");
+			foreach (string city in groupCities)
+			{
+				Console.WriteLine(city);
+			}
+		}
+	}
+}
