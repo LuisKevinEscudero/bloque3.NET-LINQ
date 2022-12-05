caf52a52dde66f71031cb973b87656714baf33e6
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-05 17:11:57 +0100

    ex6 completado medio dificil

---
+++ ex6-replaceSubstring/ex6-replaceSubstring/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Write a query that replaces 'ea' substring with astersik (*) in given list of words.
+Expected input and output
+"learn", "current", "deal" →  "l*rn", "current", "d*l"*/
+
+namespace ReplaceSubstring
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> words = new List<string> { "near", "speak", "tonight", 
+				"weapon", "customer", "deal", "lawyer" };
+
+			var query = from word in words
+						select word.Contains("ea") ? word.Replace("ea", "*") : word;
+
+            foreach (var item in query)
+			{
+				Console.WriteLine(item);
+			}
+
+		}
+	}
+}
