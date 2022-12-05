209e97ac9562e361bb321baad7303ee4c241c622
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-05 16:32:20 +0100

    ej3 completado facil

---
+++ ex3-selectWords/ex3-selectWords/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Write a query that returns words starting with letter 'a' and ending with letter 'm'.
+Expected input and output
+"mum", "amsterdam", "bloom" → "amsterdam"*/
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace SelectWords
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> words = new List<string> { "alabam", "am", "balalam", 
+				"tara", "", "a", "axeliam", "39yo0m", "trol" };
+
+			var query = from word in words
+						where word.StartsWith('a') && word.EndsWith('m')
+						select word;
+
+
+			foreach (var item in query)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
