7c4d64722a725abeba4ba91017fe8dd80303a2ca
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-05 16:53:40 +0100

    ex4 completado

---
+++ ex4-squareGreaterThan20/ex4-squareGreaterThan20/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Write a query that returns list of numbers and their squares only if
+ square is greater than 20
+Expected input and output
+[7, 2, 30] → 7 - 49, 30 - 900*/
+
+namespace SquareGraterThan20
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> numbers = new List<int> { 3, 9, 2, 4, 6, 5, 7 };
+
+			var query= from number in numbers
+					   where number*number > 20
+					   select number;
+
+			foreach (var item in query)
+			{
+				Console.WriteLine("{0} - {1}",item, item*item);
+			}
+		}
+		
+	}
+}
