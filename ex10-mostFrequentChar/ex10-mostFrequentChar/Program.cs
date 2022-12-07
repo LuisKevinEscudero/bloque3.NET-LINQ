14e4c4f5cfdc597126e0ff6a1d6b99624f803050
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-07 12:54:44 +0100

    ex11 completado normalito

---
+++ ex10-mostFrequentChar/ex10-mostFrequentChar/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Write a query that returns most frequent character in string. 
+ * Assume that there is only one such character.
+Expected input and output
+"panda"  → 'a'
+"n093nfv034nie9"→ 'n'*/
+
+namespace MostFrequentChar
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string str = "49fjs492jfJs94KfoedK0iejksKdsj3";
+
+			var query = from c in str
+						group c by c into g
+						orderby g.Count() descending
+						select g.Key;
+
+			Console.WriteLine(query.First());
+		}
+	}
+}
---
+++ ex11-uniqueValues/ex11-uniqueValues/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Given a non-empty list of strings, return a list that contains only unique (non-duplicate) strings.
+Expected input and output
+["abc", "xyz", "klm", "xyz", "abc", "abc", "rst"] → ["klm", "rst"]*/
+
+namespace UniquesValues
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var values = new List<string> { "Hi", "Meow", "Hello", "Meow", 
+				"Hi!", "Meow", "Hi", "Bye" };
+
+			var query = from value in values
+						group value by value into g
+						where g.Count() == 1
+						select g.Key;
+
+			foreach (var item in query)
+			{
+				Console.WriteLine(item);
+			}
+
+		}
+	}
+}
