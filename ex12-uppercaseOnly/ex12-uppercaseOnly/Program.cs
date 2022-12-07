719598c6787516e25062f4cdf44f624afe32e2ab
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-07 13:13:31 +0100

    ex12 completado facil

---
+++ ex12-uppercaseOnly/ex12-uppercaseOnly/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Write a query that returns only uppercase words from string.
+Expected input and output
+"DDD example CQRS Event Sourcing" → DDD, CQRS*/
+
+namespace UppercaseOnly
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string word = "THIS is UPPERCASE string LOL";
+
+			var uppercaseWords = word.Split(' ');
+
+			var query = from w in uppercaseWords
+						where w == w.ToUpper()
+						select w;
+
+			foreach (var item in query)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
