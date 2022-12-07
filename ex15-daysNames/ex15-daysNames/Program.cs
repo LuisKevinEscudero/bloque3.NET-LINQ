45028fce91b599c1f721c734fef139c3f20fd4b3
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-07 16:27:00 +0100

    ex15 no entiendo esa sentencia

---
+++ ex15-daysNames/ex15-daysNames/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Write a query that returns names of days.
+Expected input and output
+daysNames → "Sunday Monday Tuesday Wednesday Thursday Friday Saturday"*/
+
+namespace DaysNames
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var daysNames = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>();
+
+			foreach (var day in daysNames)
+			{
+				Console.WriteLine(day);
+			}
+		}
+	}
+}
